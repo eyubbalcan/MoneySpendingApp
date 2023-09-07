36660c1e6be9b7a20d7032e493388379cda0815b
eyubbalcan <126914485+eyubbalcan@users.noreply.github.com>
2023-09-07 17:01:19 +0300

    Alıştırma

---
+++ my-app/src/companents/Header.js
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+
+const Header = ({money}) => {
+  return (
+    <div>
+        Harcamak içn ${money} var.
+
+    </div>
+  )
+}
+
+export default Header
---
+++ my-app/src/companents/Product.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const Product = ({ product, basket, setBasket }) => {
+
+
+  const addBasket = () => {
+
+    const checkBasket = basket.find(item => item.id === product.id)
+    setBasket([...basket, product])
+
+    if (checkBasket) {
+      checkBasket.amount += 1
+      setBasket([...basket.filter(item => item.id !== product.id), checkBasket]);
+    } else {
+      setBasket([...basket,
+      {
+        id: product.id,
+        amount: 1
+      }])
+    }
+  }
+
+  // const removeBasket = () => {
+  //   const currentBasket = basket.find(item => item.id === product.id);
+  //   const basketWithoutCurrent = basket.filter(item => item.id !== product.id);
+  //   currentBasket.amount -= 1;
+  //   if (currentBasket === 0) {
+  //     setBasket([...basketWithoutCurrent]);
+
+  //   } else {
+  //     setBasket([...basketWithoutCurrent, currentBasket]);
+
+  //   }
+
+
+  // }
+
+
+
+
+  return (
+    <>
+      <div className="product">
+        <h6>{product.title}</h6>
+        <h3>$ {product.price}</h3>
+        <button >Sat</button>
+        <span className="amount">0</span>
+        <button onClick={addBasket}>Satın Al</button>
+
+        <style>
+          {`
+            .product {
+              padding: 10px;
+              background: #fff;
+              border: 1px solid #ddd;
+              margin-bottom: 20px;
+            }
+          `}
+        </style>
+      </div >
+    </>
+  );
+};
+
+export default Product;
